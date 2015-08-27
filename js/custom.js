3a62278afabd510458dbb3e4305a6cf0a4f9f1d4
ashikur11 <ashik365@hotmail.com>
2015-08-27 18:01:19 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+(function ($) {
+
+	//create custom functions
+
+
+	//load function while document ready
+	$(document).on('ready', function () {
+
+	});
+	//load function while window load completly
+	$(window).on('load', function () {
+
+	});
+	// load functions while window scroll
+	$(window).on('scroll', function () {
+		
+	});
+})(jQuery);
